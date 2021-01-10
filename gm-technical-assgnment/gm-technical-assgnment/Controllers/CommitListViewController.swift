534ff2dde69f29dbcd375cf26c48a19972e02ad0
jkolp <jkolp.dev@gmail.com>
2021-01-09 17:53:09 -0800

    Set the main collection view delegate to CommitsListViewController.

--- gm-technical-assgnment/gm-technical-assgnment/Controllers/CommitListViewController.swift
+++ gm-technical-assgnment/gm-technical-assgnment/Controllers/CommitListViewController.swift
@@ -13,9 +13,11 @@ class CommitListViewController: BaseViewController<CommitListView> {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         baseView.collectionView.dataSource = self
-        
+        baseView.collectionView.delegate = self
     }
 }
+
+// MARK: - UICollectionViewDataSource
 
 extension CommitListViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -26,8 +28,20 @@ extension CommitListViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = baseView.collectionView.dequeueReusableCell(withReuseIdentifier: CommitCell.cellIdentifier, for: indexPath) as! CommitCell
         cell.backgroundColor = UIColor.blue
+        
         return cell
     }
 }
 
+// MARK: - UICollectionViewDelegateFlowLayout
 
+extension CommitListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = view.frame.width
+        var height = CGFloat(200)
+        
+        return CGSize(width: width, height: height)
+        
+    }
+}
