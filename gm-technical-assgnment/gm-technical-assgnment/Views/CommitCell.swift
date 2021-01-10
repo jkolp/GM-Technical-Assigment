0c53390d8a48c629c167aed5b2a52aee28d6cc18
jkolp <jkolp.dev@gmail.com>
2021-01-09 16:18:11 -0800

    Created a custom UICollectionViewCell, CommitCell, to be used in CommitListView.

---
+++ gm-technical-assgnment/gm-technical-assgnment/Views/CommitCell.swift
@@ -0,0 +1,26 @@
+//
+//  CommitListCell.swift
+//  gm-technical-assgnment
+//
+//  Created by Projects on 1/9/21.
+//
+
+import UIKit
+
+class CommitCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - LifeCycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    
+}
